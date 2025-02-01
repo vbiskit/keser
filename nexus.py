df8f42040fd9327580edd6d733fb6a9dee55e9bd
biskit <guleycontact@gmail.com>
2025-02-02 03:43:20 +1000

    Update nexus.py

--- nexus.py
+++ nexus.py
@@ -256,6 +256,7 @@ websites = {
     "openstreetmap": "https://www.openstreetmap.org/user/{}",
     "packagist": "https://packagist.org/packages/{}/",
     "twitterstatus":"https://web.archive.org/web/*/https://twitter.com/{}/status/*",
+    "paypal": "https://www.paypal.com/paypalme/{},",
 }
 def search_username(username, threads=500):
     print(f"\033[38;2;0;255;0m[\033[38;2;255;255;0m*\033[38;2;0;255;0m]\033[38;2;0;255;0m Checking username {username} on: \033[0m\n")
