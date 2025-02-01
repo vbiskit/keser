9f97466302c935322d085a82c7315846d38cd883
biskit <guleycontact@gmail.com>
2025-02-02 02:30:27 +1000

    Update nexus.py

--- nexus.py
+++ nexus.py
@@ -6,7 +6,6 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 import random
 import os
 from colorama import Fore
-
 user_agents = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Version/14.1.2 Safari/537.36",
@@ -44,47 +43,32 @@ def scrape_duckduckgo_links(query):
     except requests.exceptions.RequestException as e:
         print(f"Error with DuckDuckGo request: {e}")
         return []
-
-def loading_screen(): 
-    sys.stdout.write("\033c")
-    print("""\033[38;2;0;0;255m                                
- ███▄    █ ▓█████ ▒██   ██▒ █    ██   ██████ 
- ██ ▀█   █ ▓█   ▀ ▒▒ █ █ ▒░ ██  ▓██▒▒██    ▒ 
-▓██  ▀█ ██▒▒███   ░░  █   ░▓██  ▒██░░ ▓██▄   
-▓██▒  ▐▌██▒▒▓█  ▄  ░ █ █ ▒ ▓▓█  ░██░  ▒   ██▒
-▒██░   ▓██░░▒████▒▒██▒ ▒██▒▒▒█████▓ ▒██████▒▒
-░ ▒░   ▒ ▒ ░░ ▒░ ░▒▒ ░ ░▓ ░░▒▓▒ ▒ ▒ ▒ ▒▓▒ ▒ ░
-░ ░░   ░ ▒░ ░ ░  ░░░   ░▒ ░░░▒░ ░ ░ ░ ░▒  ░ ░
-   ░   ░ ░    ░    ░    ░   ░░░ ░ ░ ░  ░  ░  
-         ░    ░  ░ ░    ░     ░           ░                                                                                                                                                                        
-\033[0m""")  
-    sys.stdout.flush()
-    for _ in range(1):
-        time.sleep(0.6)
-        sys.stdout.write("\033[38;2;0;255;255m@biskit")
-        sys.stdout.flush()
-    print("\n")
-
 def check_username_on_website(url, username):
     try:
         response = requests.get(url.format(username))  
         if response.status_code == 429:
             print(f"\033[38;2;255;255;255m[\033[38;2;255;0;255m~\033[38;2;255;255;255m]\033[38;2;255;0;255m To Many Request (429) {url.format(username)} \033[38;2;255;255;255mCan't Check If Link Is Found \033[0m")
+            return None
         elif response.status_code == 404:
             print(f"\033[38;2;255;255;255m[\033[38;2;255;0;0m-\033[38;2;255;255;255m]\033[\033[38;2;255;0;0m Not Found\033[38;2;255;255;255m {url.format(username)}\033[0m")
+            return None
         elif response.status_code == 410:
             print(f"\033[38;2;255;255;255m[\033[38;2;255;255;0m!\033[38;2;255;255;255m]\033[38;2;255;255;0m Name Doesn't Exist\033[38;2;255;255;255m {url.format(username)}\033[0m")  
+            return None
         elif response.status_code == 200:
             if username.lower() in response.text.lower():
                 print(f"\033[38;2;255;255;255m[\033[38;2;0;255;0m+\033[38;2;255;255;255m]\033[38;2;0;255;0m Found\033[38;2;255;255;255m {url.format(username)}\033[0m")
+                return url.format(username)  # Return the URL if the username is found
     except requests.exceptions.RequestException as e:
         print(f"Error checking {url.format(username)}: {e}")
+        return None
+    return None  
 
 websites = {
-    "GitHub": "https://github.com/{}",
+"GitHub": "https://github.com/{}",
     "Twitter": "https://x.com/{}",
     "Instagram": "https://www.instagram.com/{}",
-    "YouTube": "https://www.youtube.com/{}",
+    "YouTube": "https://www.youtube.com/@{}",
     "Reddit": "https://www.reddit.com/user/{}",
     "Duolingo": "https://www.duolingo.com/profile/{}",
     "Chess": "https://www.chess.com/member/{}",
@@ -135,7 +119,7 @@ websites = {
     "devinart": "https://www.deviantart.com/{}",
     "rblxtrade":"https://rblx.trade/p/{}",
     "kick":"https://www.kick.com/{}",
-    "steam":"https://steamcommunity.com/id/{}",
+    "steam":"https://steamcommunity.com/id/{}/",
     "Shopify":"https://{}.myshopify.com",
     "steamgifts":"https://www.steamgifts.com/user/{}",
     "venmo":"https://account.venmo.com/u/{}",
@@ -195,10 +179,86 @@ websites = {
     "dribble" : "https://dribbble.com/{}",
     "wordpress users ": "https://profiles.wordpress.org/{}",
     "linktree": "https://linktr.ee/{}",
+    "osu":"https://osu.ppy.sh/users/{}",
+    "medium":"https://medium.com/@{}",
+    "Fadom":"https://community.fandom.com/wiki/User:{}",
+    "cashapp":"https://cash.app/${}",
+    "slideshare":"https://www.slideshare.net/{}",
+    "airpilot": "https://airlinepilot.life/u/{}",
+    "airbit": "https://airbit.com/{}",
+    "airliners": "https://www.airliners.net/user/{}/profile/photos",
+    "anilist": "https://anilist.co/user/{}/",
+    "asciinema": "https://asciinema.org/~{}",
+    "atcoder": "https://atcoder.jp/users/{}",
+    "bandcamp": "https://www.bandcamp.com/{}",
+    "behance": "https://www.behance.net/{}",
+    "bezuzyteczna": "https://bezuzyteczna.pl/uzytkownicy/{}",
+    "bitbucket": "https://bitbucket.org/{}/",
+    "boardgamegeek": "https://boardgamegeek.com/user/{}",
+    "bookcrossing": "https://www.bookcrossing.com/mybookshelf/{}/",
+    "bravecommunity": "https://community.brave.com/u/{}/",
+    "carbonmade": "https://{}.carbonmade.com",
+    "chaos": "https://chaos.social/@{}",
+    "clapper": "https://clapperapp.com/{}",
+    "clozemaster": "https://www.clozemaster.com/players/{}",
+    "codeforces": "https://codeforces.com/profile/{}",
+    "codersrank": "https://profile.codersrank.io/user/{}/",
+    "coderwall": "https://coderwall.com/{}",
+    "crevado": "https://{}.crevado.com",
+    "crowdin": "https://crowdin.com/profile/{}",
+    "cults3d": "https://cults3d.com/en/users/{}/creations",
+    "cyberdefenders": "https://cyberdefenders.org/p/{}",
+    "devcommunity": "https://dev.to/{}",
+    "dmoj": "https://dmoj.ca/user/{}",
+    "dealabs": "https://www.dealabs.com/profile/{}",
+    "discuss_elastic": "https://discuss.elastic.co/u/{}",
+    "disqus": "https://disqus.com/{}",
+    "eintracht": "https://community.eintracht.de/fans/{}",
+    "empretienda": "https://{}.empretienda.com.ar",
+    "exophase": "https://www.exophase.com/user/{}",
+    "fanpop": "https://www.fanpop.com/fans/{}",
+    "fosstodon": "https://fosstodon.org/@{}",
+    "gnomevcs": "https://gitlab.gnome.org/{}",
+    "gaiaonline": "https://www.gaiaonline.com/profiles/{}",
+    "gamespot": "https://www.gamespot.com/profile/{}/",
+    "geeksforgeeks": "https://auth.geeksforgeeks.org/user/{}",
+    "giantbomb": "https://www.giantbomb.com/profile/{}/",
+    "grailed": "https://www.grailed.com/{}",
+    "hackthebox": "https://forum.hackthebox.com/u/{}",
+    "hackernews": "https://news.ycombinator.com/user?id={}",
+    "hackerone": "https://hackerone.com/{}",
+    "hashnode": "https://hashnode.com/@{}",
+    "holopin": "https://holopin.io/@{}",
+    "ifttt": "https://www.ifttt.com/p/{}",
+    "imgur": "https://imgur.com/user/{}",
+    "issuu": "https://issuu.com/{}",
+    "itch": "https://{}.itch.io/",
+    "itemfix": "https://www.itemfix.com/c/{}",
+    "joplin": "https://discourse.joplinapp.org/u/{}/",
+    "kaskus": "https://www.kaskus.co.id/@{}",
+    "keybase": "https://keybase.io/{}",
+    "kongregate": "https://www.kongregate.com/accounts/{}",
+    "launchpad": "https://launchpad.net/~{}",
+    "lesswrong": "https://www.lesswrong.com/users/@{}",
+    "lichess": "https://lichess.org/@/{}/",
+    "listed": "https://listed.to/@{}",
+    "livejournal": "https://{}.livejournal.com",
+    "mmorpg": "https://forums.mmorpg.com/profile/{}",
+    "memrise": "https://www.memrise.com/user/{}/",
+    "mixcloud": "https://www.mixcloud.com/{}/",
+    "monkeytype": "https://monkeytype.com/profile/{}",
+    "myanimelist": "https://myanimelist.net/profile/{}",
+    "mydramalist": "https://www.mydramalist.com/profile/{}",
+    "myspace": "https://myspace.com/{}",
+    "nicommunity": "https://community.native-instruments.com/profile/{}",
+    "nationstates": "https://nationstates.net/nation={}",
+    "naver": "https://blog.naver.com/{}",
+    "nintendolife": "https://www.nintendolife.com/users/{}",
+    "nitrotype": "https://www.nitrotype.com/racer/{}",
+    "openstreetmap": "https://www.openstreetmap.org/user/{}",
+    "packagist": "https://packagist.org/packages/{}/",
 }
-
-def search_username(username, threads=500,):
-    
+def search_username(username, threads=500):
     print(f"\033[38;2;0;255;0m[\033[38;2;255;255;0m*\033[38;2;0;255;0m]\033[38;2;255;255;255m Checking username {username} \033[0m\n")
 
     found = set()  
@@ -209,25 +269,52 @@ def search_username(username, threads=500,):
             futures.append(executor.submit(check_username_on_website, url, username))
         
         for future in as_completed(futures):
-            future.result()
+            result = future.result()
+            if result:  # Ensure result is valid
+                found.add(result)
 
     duckduckgo_results = scrape_duckduckgo_links(username)
 
+    found_count = len(found)  # Count of main website results (excluding DuckDuckGo)
+    duckduckgo_count = len(duckduckgo_results)
+    total_found_links = found_count + duckduckgo_count
+
     if found or duckduckgo_results:
-        
-        print("\033[38;2;0;255;0m[\033[38;2;255;255;0m*\033[38;2;0;255;0m]\033[38;2;0;255;0m Found the username on the following websites:\033[0m")
-        for result in found:
-            print(f"\033[38;2;255;0;255m{result}\033[0m")  
+        if found:
+            print(f"\033[38;2;0;255;0m[\033[38;2;255;255;0m*\033[38;2;0;255;0m]\033[38;2;0;255;0m Found the username on {found_count} websites:\033[0m")
+            for result in found:
+                print(f"\033[38;2;255;255;0m{result}\033[0m")  
 
         if duckduckgo_results:
-            print("\n\033[38;2;0;255;0m[\033[38;2;255;255;0m*\033[38;2;0;255;0m]\033[38;2;255;255;255m Found the username in DuckDuckGo search results:\033[0m")
+            print(f"\n\033[38;2;0;255;0m[\033[38;2;255;255;0m*\033[38;2;0;255;0m]\033[38;2;0;255;0m Found the username in DuckDuckGo search results ({duckduckgo_count} links):\033[0m")
             for duckduckgo_result in duckduckgo_results:
-                print(f"\033[\033[38;2;255;255;0m{duckduckgo_result}\033[0m") 
+                print(f"\033[38;2;255;255;255m{duckduckgo_result}\033[0m")
+
+        print(f"\n\033[38;2;0;255;0m[\033[38;2;255;255;0m*\033[38;2;0;255;0m]\033[38;2;255;255;255m Total website links found: {found_count}\033[0m")
+        print(f"\033[38;2;0;255;0m[\033[38;2;255;255;0m*\033[38;2;0;255;0m]\033[38;2;255;255;255m Total links found including DuckDuckGo: {total_found_links}\033[0m")
+
     else:
         print(f"\n\033[38;2;255;69;0mNo exact matches for '{username}' were found on the listed websites or DuckDuckGo.\033[0m")
 
 if __name__ == "__main__":
-    loading_screen()
-    username = input("\033[38;2;0;0;255m[\033[38;2;255;255;255m*\033[38;2;0;0;255m]\033[\033[38;2;255;255;255m Enter Persons Name \033[0m")  
+    sys.stdout.write("\033c")
+    print("""\033[38;2;0;0;255m                                
+ ███▄    █ ▓█████ ▒██   ██▒ █    ██   ██████ 
+ ██ ▀█   █ ▓█   ▀ ▒▒ █ █ ▒░ ██  ▓██▒▒██    ▒ 
+▓██  ▀█ ██▒▒███   ░░  █   ░▓██  ▒██░░ ▓██▄   
+▓██▒  ▐▌██▒▒▓█  ▄  ░ █ █ ▒ ▓▓█  ░██░  ▒   ██▒
+▒██░   ▓██░░▒████▒▒██▒ ▒██▒▒▒█████▓ ▒██████▒▒
+░ ▒░   ▒ ▒ ░░ ▒░ ░▒▒ ░ ░▓ ░░▒▓▒ ▒ ▒ ▒ ▒▓▒ ▒ ░
+░ ░░   ░ ▒░ ░ ░  ░░░   ░▒ ░░░▒░ ░ ░ ░ ░▒  ░ ░
+   ░   ░ ░    ░    ░    ░   ░░░ ░ ░ ░  ░  ░  
+         ░    ░  ░ ░    ░     ░           ░                                                                                                                                                                        
+\033[0m""")  
+    sys.stdout.flush()
+    for _ in range(1):
+        time.sleep(0.6)
+        sys.stdout.write("\033[38;2;0;255;255m@biskit")
+        sys.stdout.flush()
+    print("\n")
+    username = input("\033[38;2;0;0;255m[\033[38;2;255;255;255m*\033[38;2;0;0;255m]\033[\033[38;2;255;255;255m Enter Person's Name \033[0m")  
     threads = int(input("\033[38;2;0;0;255m[\033[38;2;255;255;255m*\033[38;2;0;0;255m]\033[\033[38;2;255;255;255m Enter Number of Threads (1-500) \033[0m"))
     search_username(username, threads)
