251b5422c287d8e788f92f4986e3d7110bf6890a
biskit <guleycontact@gmail.com>
2025-02-02 03:45:00 +1000

    Update nexus.py

--- nexus.py
+++ nexus.py
@@ -257,6 +257,7 @@ websites = {
     "packagist": "https://packagist.org/packages/{}/",
     "twitterstatus":"https://web.archive.org/web/*/https://twitter.com/{}/status/*",
     "paypal": "https://www.paypal.com/paypalme/{},",
+    "kik":"https://kik.me/{}",
 }
 def search_username(username, threads=500):
     print(f"\033[38;2;0;255;0m[\033[38;2;255;255;0m*\033[38;2;0;255;0m]\033[38;2;0;255;0m Checking username {username} on: \033[0m\n")
