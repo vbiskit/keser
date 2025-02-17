6b177cb72e996e5fba9b0786fca27265419e1321
biskit <guleycontact@gmail.com>
2025-02-17 12:24:07 +1000

    Update userfinder.py

--- userfinder.py
+++ userfinder.py
@@ -7207,18 +7207,18 @@ def search_username(username, threads=500, save_file=None):
                     unique_sites.add(site_name)
                     site_metadata = next((site for site in metadata["sites"] if site["name"] == site_name), None)
                     category = site_metadata["cat"] if site_metadata else "Unknown"
-                    output += f"\033[38;2;255;255;255m[\033[38;2;230;168;255m{site_name}\033[38;2;255;255;255m] \033[38;2;255;255;255m[\033[38;5;81m{category}\033[38;2;255;255;255m]\033[38;2;255;255;255m {url}\n"
+                    output += f"\033[38;2;255;255;255m[\033[38;2;230;168;255m{site_name}\033[38;2;255;255;255m] \033[38;2;255;255;255m[\033[38;2;255;221;51m{category}\033[38;2;255;255;255m]\033[38;2;0;128;128m {url}\n"
 
         if duckduckgo_results:
-            output += f"\n\033[38;2;255;0;255mDuckDuckGo\n"
+            output += f"\n\033[38;2;230;168;255mDuckDuckGo\n"
             for i, link in enumerate(duckduckgo_results, 1):
-                output += f"\033[38;2;255;255;255m[\033[38;2;0;128;128m{i}\033[38;2;255;255;255m] \033[\033[38;2;255;221;51m{link}\n"
+                output += f"\033[38;2;255;255;255m[\033[38;2;255;221;51m{i}\033[38;2;255;255;255m] \033[\033[38;2;0;128;128m{link}\n"
 
         output += f"\n\033[38;2;255;255;255m[\033[38;2;0;255;0m+\033[38;2;255;255;255m] Websites found: \033[38;2;0;255;0m{len(found)}\n"
-        output += f"\033[38;2;255;255;255m[\033[38;2;0;255;0m+\033[38;2;255;255;255m] Time Taken: \033[38;2;0;255;0m{elapsed_time:.2f} \033[38;2;255;255;255mseconds\n"
+        output += f"\033[38;2;255;255;255m[\033[38;2;255;130;0m*\033[38;2;255;255;255m] Time Taken: \033[38;2;0;255;0m{elapsed_time:.2f} \033[38;2;255;255;255mseconds\n"
     else:
         output += f"\n\033[38;2;255;255;255m[\033[38;2;255;255;0m!\033[38;2;255;255;255m]\033[38;2;255;0;0m No matches found\n"
-        output += f"\033[38;2;255;255;255m[\033[38;2;0;255;0m+\033[38;2;255;255;255m] Time Taken: \033[38;2;0;255;0m{elapsed_time:.2f} \033[38;2;255;255;255mseconds\n"
+        output += f"\033[38;2;255;255;255m[\033[38;2;255;130;0m*\033[38;2;255;255;255m] Time Taken: \033[38;2;0;255;0m{elapsed_time:.2f} \033[38;2;255;255;255mseconds\n"
 
     if save_file:
         try:
@@ -7245,7 +7245,7 @@ Usage:
     print(help_text)
 
 if __name__ == "__main__":
-    color_blue = "\033[38;2;173;216;230m"  
+    color_purple = "\033[38;2;255;0;255m"  
     color_white = "\033[38;2;255;255;255m"  
     
     logo = r"""
@@ -7256,7 +7256,7 @@ if __name__ == "__main__":
 \__,_/____/\___/_/  /_/ /_/_/ /_/\__,_/\___/_/     
                                                                                      
 """
-    print(f"{color_blue}{logo}")
+    print(f"{color_purple}{logo}")
     print(f"{color_white}                              Coded By BisKit\n")
 
     if len(sys.argv) < 2:
